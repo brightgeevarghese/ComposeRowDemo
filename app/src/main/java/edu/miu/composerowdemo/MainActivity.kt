8c63132fa7f46f0001dc29f9f0b4785ecf543ea2
Bright Gee Varghese <brightlsom@gmail.com>
2024-07-06 15:16:31 -0500

    Updated RowText

--- app/src/main/java/edu/miu/composerowdemo/MainActivity.kt
+++ app/src/main/java/edu/miu/composerowdemo/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import edu.miu.composerowdemo.ui.theme.ComposeRowDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,32 +30,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeRowDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Row(
-                        modifier = Modifier.fillMaxSize()
+                    RowText(
+                        modifier = Modifier
                             .padding(innerPadding)
-                            .background(Color.LightGray),
-                        horizontalArrangement = Arrangement.SpaceEvenly,
-                        verticalAlignment = Alignment.Top
-                    ) {
-                        MyText(
-                            text = "Fairfield",
-                            modifier = Modifier
-                                .padding(innerPadding)
-                                .background(Color(0xFF89C984))
-                        )
-                        MyText(
-                            text = "Iowa",
-                            modifier = Modifier
-                                .padding(innerPadding)
-                                .background(Color(0xFF03A9F4))
-                        )
-                        MyText(
-                            text = "USA",
-                            modifier = Modifier
-                                .padding(innerPadding)
-                                .background(Color(0xFFF2F3F5))
-                        )
-                    }
+                    )
                 }
             }
         }
@@ -62,61 +41,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyText(text: String, modifier: Modifier = Modifier) {
-    Text(
-        text = text,
+fun RowText(modifier: Modifier = Modifier) {
+    Row(
         modifier = modifier
-    )
+            .fillMaxWidth()
+            .background(Color.LightGray),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.Top
+    ) {
+        Text(
+            text = "Fairfield",
+            modifier = modifier
+                .background(Color(0xFF89C984)),
+            fontSize = 20.sp
+        )
+        Text(
+            text = "Iowa",
+            modifier = modifier
+                .background(Color(0xFF03A9F4)),
+            fontSize = 20.sp
+        )
+        Text(
+            text = "USA",
+            modifier = modifier
+                .background(Color(0xFFF2F3F5)),
+            fontSize = 20.sp
+        )
+    }
 }
 
 @Preview(showBackground = false)
 @Composable
 fun MyTextPreview() {
-    Row(
-        modifier = Modifier
-            .background(Color.LightGray)
-            .padding(50.dp)
-            .fillMaxSize(),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-//        horizontalArrangement = Arrangement.SpaceBetween,
-//        horizontalArrangement = Arrangement.SpaceAround,
-//        horizontalArrangement = Arrangement.spacedBy(16.dp),
-//        horizontalArrangement = Arrangement.Start,
-//        horizontalArrangement = Arrangement.End,
-        verticalAlignment = Alignment.Top,
-//        verticalAlignment = Alignment.Bottom,
-//        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        MyText(
-            text = "Fairfield",
-            modifier = Modifier
-                .background(Color(0xFF89C984))
-        )
-        MyText(
-            text = "Iowa",
-            modifier = Modifier
-                .background(Color(0xFF03A9F4))
-        )
-        MyText(
-            text = "USA",
-            modifier = Modifier
-                .background(Color(0xFFF6F6F7))
-        )
-    }
+    RowText()
 }
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    ComposeRowDemoTheme {
-//        Greeting("Android")
-//    }
-//}
